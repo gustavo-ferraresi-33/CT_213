f74598479699c896fe111c36f0f83f8e4f9ed1a7
gustavo-ferraresi-33 <57503581+gustavo-ferraresi-33@users.noreply.github.com>
2025-03-04 14:06:56 -0300

    Definition of new terms in order to ease development

--- Laboratorios/Lab_1/Arquivos_fornecidos_pelo_Manga/ct213_lab1_2025/constants.py
+++ Laboratorios/Lab_1/Arquivos_fornecidos_pelo_Manga/ct213_lab1_2025/constants.py
@@ -9,11 +9,23 @@ FREQUENCY = 60.0  # simulation frequency
 SAMPLE_TIME = 1.0 / FREQUENCY  # simulation sample time
 
 # Behavior Parameters
-MOVE_FORWARD_TIME = 3.0  # time moving forward before switching to the spiral behavior
-MOVE_IN_SPIRAL_TIME = 20.0  # time moving in spiral before switching back to moving forward
-GO_BACK_TIME = 0.5  # time going back after hitting a wall
+
+# Times relevant to DFSM state transitions
+# "t2"
+t2 = MOVE_FORWARD_TIME = 3.0  # time moving forward before switching to the spiral behavior
+n2 = t2 * FREQUENCY # number of sampling cycles elapsed in the time "t2"
+# "t1"
+t1 = MOVE_IN_SPIRAL_TIME = 20.0  # time moving in spiral before switching back to moving forward
+n1 = t1 * FREQUENCY # number of sampling cycles elapsed in the time "t1"
+# "t3"
+t3 = GO_BACK_TIME = 0.5  # time going back after hitting a wall
+n3 = t3 * FREQUENCY # number of sampling cycles elapsed in the time "t3"
+
+# Default speeds
 FORWARD_SPEED = 0.5  # default linear speed when going forward
 BACKWARD_SPEED = -0.1 # default backward speed when going back after hitting a wall
-INITIAL_RADIUS_SPIRAL = 0.2  # initial spiral radius
-SPIRAL_FACTOR = 0.05  # factor used to make the spiral grow while the time passes
 ANGULAR_SPEED = 0.5  # default angular speed
+
+# Spiral geometric parameters
+r0 = INITIAL_RADIUS_SPIRAL = 0.2  # initial spiral radius
+b = SPIRAL_FACTOR = 0.05  # factor used to make the spiral grow while the time passes
