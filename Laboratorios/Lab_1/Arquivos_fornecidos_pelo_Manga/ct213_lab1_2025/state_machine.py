744e457553606ef28142cff25ecc37dd65948c6c
gustavo-ferraresi-33 <57503581+gustavo-ferraresi-33@users.noreply.github.com>
2025-03-10 21:46:36 -0300

    Write the methods of all 4 "State" class's daugther classes

--- Laboratorios/Lab_1/Arquivos_fornecidos_pelo_Manga/ct213_lab1_2025/state_machine.py
+++ Laboratorios/Lab_1/Arquivos_fornecidos_pelo_Manga/ct213_lab1_2025/state_machine.py
@@ -28,25 +28,27 @@ class FiniteStateMachine(object):
         new_state = State(new_state_name)
         self.state = new_state
 
-    def update(self, agent: Roomba, clock: Clock):
-        self.state.check_transition(agent, self, clock)
-        self.state.execute(agent, clock)
+    def update(self, agent: Roomba):
+        self.state.check_transition(agent, self)
+        self.state.execute(agent)
 
 
 class State(object):
     """
     Abstract state class.
     """
-    def __init__(self, state_name: StateName):
+    def __init__(self, n: int = 0, *, state_name: StateName):
         """
         Creates a state.
 
         :param state_name: the name of the state.
         :type state_name: str
         """
+        # Initialize sampling cycles counter "n"
+        self.n = n
         self.state_name = state_name
 
-    def check_transition(self, agent: Roomba, fsm: FiniteStateMachine, clock: Clock):
+    def check_transition(self, agent: Roomba, fsm: FiniteStateMachine):
         """
         Checks conditions and execute a state transition if needed.
 
@@ -55,7 +57,7 @@ class State(object):
         """
         raise NotImplementedError("This method is abstract and must be implemented in derived classes")
 
-    def execute(self, agent: Roomba, clock: Clock):
+    def execute(self, agent: Roomba):
         """
         Executes the state logic.
 
@@ -66,47 +68,53 @@ class State(object):
 
 class MoveForwardState(State):
     def __init__(self):
+        super().__init__(state_name = StateName.MOVE_FORWARD)
         # TODO: add initialization code
-        super().__init__(StateName.MOVE_FORWARD)
-        # DUVIDA: did I forget to initialize something?
+        # Set sampling cycles counter "n" to "0"
+        self.n = 0
 
-    def check_transition(self, agent: Roomba, state_machine: FiniteStateMachine, clock: Clock):
+    def check_transition(self, agent: Roomba, state_machine: FiniteStateMachine):
         # TODO: add logic to check and execute state transition
         # If a collision is detected
         if agent.get_bumper_state():
             # Go to the state GoBackState
             state_machine.change_state(StateName.GO_BACK)
-        # If maximum time t2 is surpassed
-        elif clock.get_rawtime() >= t2:
+        # If the maximum number of sampling cycles n2 is surpassed
+        elif self.n >= n2:
             # Go to the state MoveInSpiral
             state_machine.change_state(StateName.MOVE_IN_SPIRAL)
 
-    def execute(self, agent: Roomba, clock: Clock):
+    def execute(self, agent: Roomba):
         # TODO: add execution logic
+        # Increment sampling cycles counter "n"
+        self.n += 1
         # Change velocity to move forwards
         agent.set_velocity(FORWARD_SPEED, 0)
 
 class MoveInSpiralState(State):
     def __init__(self):
         # TODO: add initialization code
-        super().__init__(StateName.MOVE_IN_SPIRAL)
-        # DUVIDA: did I forget to initialize something?
+        super().__init__(state_name = StateName.MOVE_IN_SPIRAL)
+        # Set sampling cycles counter "n" to "0"
+        self.n = 0
 
-    def check_transition(self, agent: Roomba, state_machine: FiniteStateMachine, clock: Clock):
+    def check_transition(self, agent: Roomba, state_machine: FiniteStateMachine):
         # TODO: add logic to check and execute state transition
         # If a collision is detected
         if agent.get_bumper_state():
             # Go to the state GoBackState
             state_machine.change_state(StateName.GO_BACK)
-        # If maximum time t1 is surpassed
-        elif clock.get_rawtime() >= t1:
+        # If the maximum number of sampling cycles n1 is surpassed
+        elif self.n >= n1:
             # Go to the state MoveInSpiral
             state_machine.change_state(StateName.MOVE_IN_SPIRAL)            
 
-    def execute(self, agent: Roomba, clock: Clock):
+    def execute(self, agent: Roomba):
         # TODO: add execution logic
+        # Increment sampling cycles counter "n"
+        self.n += 1
         # Instantaneous curvature radius
-        r = r0 + b * clock.get_rawtime()
+        r = r0 + b * self.n * SAMPLE_TIME
         # Change velocity to move in spiral
         agent.set_velocity(FORWARD_SPEED, FORWARD_SPEED/r)
 
@@ -115,36 +123,42 @@ class MoveInSpiralState(State):
 class GoBackState(State):
     def __init__(self):
         # TODO: add initialization code
-        super().__init__(StateName.GO_BACK)
-        # DUVIDA: did I forget to initialize something?
+        super().__init__(state_name = StateName.GO_BACK)
+        # Set sampling cycles counter "n" to "0"
+        self.n = 0
 
-    def check_transition(self, agent: Roomba, state_machine: FiniteStateMachine, clock: Clock):
+    def check_transition(self, agent: Roomba, state_machine: FiniteStateMachine):
         # TODO: add logic to check and execute state transition
-        # If maximum time t3 is surpassed
-        if clock.get_rawtime() >= t3:
+        # If the maximum number of sampling cycles n3 is surpassed
+        if self.n >= n3:
             # Go to the state MoveInSpiral
             state_machine.change_state(StateName.ROTATE)            
         
-    def execute(self, agent: Roomba, clock: Clock):
+    def execute(self, agent: Roomba):
         # TODO: add execution logic
+        # Increment sampling cycles counter "n"
+        self.n += 1
         # Change velocity to move backwards
         agent.set_velocity(-BACKWARD_SPEED, 0)
         # DUVIDA: call "check_transition()" ?
-        
+
 
 class RotateState(State):
     def __init__(self):
         # TODO: add initialization code
-        super().__init__(StateName.ROTATE)
-        # DUVIDA: did I forget to initialize something?
+        super().__init__(state_name = StateName.ROTATE)
+        # Set sampling cycles counter "n" to "0"
+        self.n = 0
 
-    def check_transition(self, agent: Roomba, state_machine: FiniteStateMachine, clock: Clock):
+    def check_transition(self, agent: Roomba, state_machine: FiniteStateMachine):
         # TODO: add logic to check and execute state transition
-        # Go to the state MoveInSpiral
+        # Go to the state MoveForward
         state_machine.change_state(StateName.MOVE_FORWARD)
     
-    def execute(self, agent: Roomba, clock: Clock):
+    def execute(self, agent: Roomba):
         # TODO: add execution logic
+        # Increment sampling cycles counter "n"
+        self.n += 1
         # Spin of a random angle in [-pi; pi) with uniform PDF
         agent.pose.rotation += math.pi * random.uniform(-1, 1)
 
