b2f61b0340a6d3dbcdf22cdf71f6bc5aa657089b
gustavo-ferraresi-33 <57503581+gustavo-ferraresi-33@users.noreply.github.com>
2025-03-16 05:16:17 -0300

    Removed code lines intended for debugging, as well as eliminated unecessary function calls.

--- Laboratorios/Lab_1/Arquivos_fornecidos_pelo_Manga/ct213_lab1_2025/behavior_tree.py
+++ Laboratorios/Lab_1/Arquivos_fornecidos_pelo_Manga/ct213_lab1_2025/behavior_tree.py
@@ -4,7 +4,6 @@ from constants import *
 import random
 import math
 
-import inspect
 from Arquivos_fornecidos_pelo_Manga.ct213_lab1_2025.roomba import main_phase
 
 class NodeStatus(Enum):
@@ -89,9 +88,6 @@ class LeafNode(TreeNode):
     def __init__(self, status = None, node_name = "No name attributed", parent = None, n = 0):
         super().__init__(status = status, node_name = node_name, parent = parent, n = n)
 
-        # # DEBUGGING: printing leaf node being executed
-        # print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
 
 class CompositeNode(TreeNode):
     """
@@ -101,9 +97,6 @@ class CompositeNode(TreeNode):
         super().__init__(status = status, node_name = node_name, parent = parent, n = n)
         self.children = []
 
-        # # DEBUGGING: printing leaf node being executed
-        # print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
 
     def add_child(self, child):
         """
@@ -112,9 +105,6 @@ class CompositeNode(TreeNode):
         :param child: child to be added to this node.
         :type child: TreeNode
         """
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
 
         child.parent = self
         self.children.append(child)
@@ -129,29 +119,20 @@ class SequenceNode(CompositeNode):
         # We need to keep track of the last running child when resuming the tree execution
         self.running_child = None
 
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
 
     def enter(self, agent = None):
         # When this node is entered, no child should be running
         self.running_child = None
-
+        # Change node status to VISITING
         self.status = NodeStatus.VISITING
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
 
 
     def execute(self, agent):
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         if self.running_child is None:
             # If a child was not running, then the node puts its first child to run
             self.running_child = self.children[0]
             self.running_child.enter(agent)
+
         loop = True
         while loop:
             # Execute the running child
@@ -187,27 +168,20 @@ class SelectorNode(CompositeNode):
         # We need to keep track of the last running child when resuming the tree execution
         self.running_child = None
 
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
 
     def enter(self, agent = None):
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
         # When this node is entered, no child should be running
         self.running_child = None
+        # Change node status to VISITING
         self.status = NodeStatus.VISITING
 
 
     def execute(self, agent):
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         if self.running_child is None:
             # If a child was not running, then the node puts its first child to run
             self.running_child = self.children[0]
             self.running_child.enter(agent)
+
         loop = True
         while loop:
             # Execute the running child
@@ -240,9 +214,6 @@ class RoombaBehaviorTree(BehaviorTree):
     def __init__(self):
         # Todo: construct the tree here
 
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Creating root node (Selector)
         selector_root = SelectorNode(node_name = "selector_root", parent = None, n = 0)
 
@@ -251,7 +222,7 @@ class RoombaBehaviorTree(BehaviorTree):
         # Creating left subtree's leaf nodes
         move_forward = MoveForwardNode(node_name = "move_forward", parent = sequence_left, n = 0)
         move_in_spiral = MoveInSpiralNode(node_name = "move_in_spiral", parent = sequence_left, n = 0)
-        # Building left subtree
+        # Building left subtree structure
         sequence_left.add_child(move_forward)
         sequence_left.add_child(move_in_spiral)
 
@@ -261,36 +232,13 @@ class RoombaBehaviorTree(BehaviorTree):
         # Creating right subtree's leaf nodes
         go_back = GoBackNode(node_name = "go_back", parent = sequence_right, n = 0)
         rotate = RotateNode(node_name = "rotate", parent = sequence_right, n = 0)
-        # Building right subtree
+        # Building right subtree structure
         sequence_right.add_child(go_back)
         sequence_right.add_child(rotate)
 
-        # Building the entire tree
+        # Building the entire tree's structure
         selector_root.add_child(sequence_left)
         selector_root.add_child(sequence_right)
-
-        # DEBUGGING: checking if the behavior tree was constructed correctly
-        def print_node_info(debug_node):
-            print(2 * "\n" + 40 * "-" + "\n", "Node under analysis", debug_node.node_name)
-
-            if debug_node.parent is not None:
-                print("Parent of node", debug_node.node_name, "is", debug_node.parent.node_name)
-
-            if hasattr(debug_node, "children"):
-                print("Children of node", debug_node.node_name, "are", [child.node_name for child in debug_node.children])
-
-            print("Node status of node", debug_node.node_name, "is", debug_node.status)
-            print("Sampling cycles counter of node", debug_node.node_name, "is n ==", debug_node.n)
-
-        print_node_info(selector_root)
-        print_node_info(sequence_left)
-        print_node_info(sequence_right)
-        print_node_info(move_forward)
-        print_node_info(move_in_spiral)
-        print_node_info(go_back)
-        print_node_info(rotate)
-        print(4 * "\n")
-
         super().__init__(root = selector_root)
 
 
@@ -298,22 +246,12 @@ class MoveForwardNode(LeafNode):
     def __init__(self, status = NodeStatus.NOT_VISITED,  node_name = "move_forward", parent = None, n = 0):
         super().__init__(status = status, node_name = node_name, parent = parent, n = n)
         # Todo: add initialization code
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Set sampling cycles counter "n" to "0"
         self.n = 0
 
 
-
-
     def enter(self, agent):
         # Todo: add enter logic
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Set node status to VISITING
         self.status = NodeStatus.VISITING
 
@@ -322,32 +260,25 @@ class MoveForwardNode(LeafNode):
     def execute(self, agent):
         # Todo: add execution logic
 
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # If leaf node is not being visited, raise error
         if self.status != NodeStatus.VISITING:
             raise ValueError("Error! Node status is not VISITING.")
 
         # Change velocity to move forwards
         agent.set_velocity(FORWARD_SPEED, 0)
-        # DUVIDA: call "check_transition()" ?
 
         # Increment sampling cycles counter "n"
         self.n += 1
         # If a collision occurred
         if agent.get_bumper_state():
-            print(type(self).__name__, "returned", ExecutionStatus.FAILURE)
             agent.set_velocity(0, 0)
             self.n = 0
             return ExecutionStatus.FAILURE
         # If no collision occurred but the movement is not finished
         elif 0 <= self.n < n2:
-            print(type(self).__name__, "returned", ExecutionStatus.RUNNING)
             return ExecutionStatus.RUNNING
         # If the roomba was able to execute the entire movement without colliding, return SUCCESS
         elif self.n >= n2:
-            print(type(self).__name__, "returned", ExecutionStatus.SUCCESS)
             self.n = 0
             self.status = NodeStatus.VISITED
             return ExecutionStatus.SUCCESS
@@ -360,29 +291,18 @@ class MoveInSpiralNode(LeafNode):
     def __init__(self, status = NodeStatus.NOT_VISITED, node_name = "move_in_spiral", parent = None, n = 0):
         super().__init__(status = status, node_name = node_name, parent = parent, n = n)
         # Todo: add initialization code
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Set sampling cycles counter "n" to "0"
         self.n = 0
 
 
     def enter(self, agent):
         # Todo: add enter logic
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Set node status to VISITING
         self.status = NodeStatus.VISITING
 
 
     def execute(self, agent):
         # Todo: add execution logic
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
 
         # If leaf node is not being visited, raise error
         if self.status != NodeStatus.VISITING:
@@ -392,23 +312,19 @@ class MoveInSpiralNode(LeafNode):
         r = r0 + b * self.n * SAMPLE_TIME
         # Change velocity to move in spiral
         agent.set_velocity(FORWARD_SPEED, FORWARD_SPEED / r)
-        # DUVIDA: call "check_transition()" ?
 
         # Increment sampling cycles counter "n"
         self.n += 1
         # If a collision occurred
         if agent.get_bumper_state():
-            print(type(self).__name__, "returned", ExecutionStatus.FAILURE)
             self.n = 0
             agent.set_velocity(0, 0)
             return ExecutionStatus.FAILURE
         # If no collision occurred but the movement is not finished
         elif 0 <= self.n < n1:
-            print(type(self).__name__, "returned", ExecutionStatus.RUNNING)
             return ExecutionStatus.RUNNING
         # If the roomba was able to execute the entire movement without colliding, return SUCCESS
         elif self.n >= n1:
-            print(type(self).__name__, "returned", ExecutionStatus.SUCCESS)
             self.n = 0
             self.status = NodeStatus.VISITED
             return ExecutionStatus.SUCCESS
@@ -420,10 +336,6 @@ class MoveInSpiralNode(LeafNode):
 class GoBackNode(LeafNode):
     def __init__(self, status = NodeStatus.NOT_VISITED, node_name = "go_back", parent = None, n = 0):
         super().__init__(status = status, node_name = node_name, parent = parent, n = n)
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Todo: add initialization code
         # Set sampling cycles counter "n" to "0"
         self.n = 0
@@ -431,10 +343,6 @@ class GoBackNode(LeafNode):
 
     def enter(self, agent):
         # Todo: add enter logic
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Set node status to VISITING
         self.status = NodeStatus.VISITING
 
@@ -442,20 +350,14 @@ class GoBackNode(LeafNode):
     def execute(self, agent):
         # Todo: add execution logic
 
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # If leaf node is not being visited, raise error
         if self.status != NodeStatus.VISITING:
             raise ValueError("Error! Node status is not VISITING.")
-
-
 
         # Change velocity to move backwards
         if int(self.n) == 0:
             agent.set_velocity(BACKWARD_SPEED, 0)
             agent.set_bumper_state(False)
-        # DUVIDA: call "check_transition()" ?
 
         # Increment sampling cycles counter "n"
         self.n += 1
@@ -468,19 +370,15 @@ class GoBackNode(LeafNode):
         # * "MoveForwardNode" and "GoBackNode"                                                            *
         # *************************************************************************************************
         if agent.get_bumper_state() or self.n >= n3:
-            print(type(self).__name__, "returned", ExecutionStatus.SUCCESS)
             self.n = 0
             self.status = NodeStatus.VISITED
             return ExecutionStatus.SUCCESS
         # If no collision occurred but the movement is not finished
         elif 0 <= self.n < n3:
-            print(type(self).__name__, "returned", ExecutionStatus.RUNNING)
             return ExecutionStatus.RUNNING
         # If none of the above options is entered
         else:
             raise ValueError("Error! Did not enter in any of the options.")
-
-
 
 
 class RotateNode(LeafNode):
@@ -488,34 +386,20 @@ class RotateNode(LeafNode):
         super().__init__(status = status, node_name = node_name, parent = parent, n = n)
         # Todo: add initialization code
         self.delta_rotation = main_phase(delta_rotation)
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Set sampling cycles counter "n" to "0"
         self.n = 0
 
 
     def enter(self, agent):
         # Todo: add enter logic
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
-
         # Set node status to VISITING
         self.status = NodeStatus.VISITING
         # Choosing a new random value for "delta_rotation"
         self.delta_rotation = math.pi * random.uniform(-1, 1)
-        # Make both roomba's velocities equal to zero
-        agent.set_velocity(0, 0)
-
 
 
     def execute(self, agent):
         # Todo: add execution logic
-
-        # DEBUGGING: printing leaf node being executed
-        print(type(self).__name__ + "." + inspect.currentframe().f_code.co_name)
 
         # If leaf node is not being visited, raise error
         if self.status != NodeStatus.VISITING:
@@ -523,30 +407,14 @@ class RotateNode(LeafNode):
 
         agent.set_bumper_state(False)
 
-        # DEBUGGING
-        print(type(self).__name__ + ": self.delta_rotation ==", self.delta_rotation)
-        # DEBUGGING
-        print(type(self).__name__ + ": agent.pose.rotation ==", agent.pose.rotation)
-        # DEBUGGING
-        print(type(self).__name__ + ": \"n\" ==", self.n)
-        # DEBUGGING
-        print(type(self).__name__ + ": number of sampling cycles to run to complete rotation ==",
-              math.ceil(math.fabs(self.delta_rotation / (ANGULAR_SPEED * SAMPLE_TIME))),
-        )
-
-        # Changing the roomba's velocity to make it rotate, in the shortest amount of time, to the
+        # Changing the roomba's velocity to make it rotate, ***IN THE SHORTEST AMOUNT OF TIME***, to the
         # desired value of "agent.pose.inclination"
         if self.delta_rotation >= 0:
-            # Spin in clockwise sense (positive w)
+            # Spin clockwise (positive w)
             agent.set_velocity(0, ANGULAR_SPEED)
         else:
-            # Spin in counterclockwise sense (negative w)
+            # Spin counterclockwise (negative w)
             agent.set_velocity(0, -ANGULAR_SPEED)
-
-
-
-        # DUVIDA: call "check_transition()" ?
-
 
         # Increment sampling cycles counter "n"
         self.n += 1
@@ -555,16 +423,11 @@ class RotateNode(LeafNode):
             raise RuntimeError("Error! Roomba collided while rotating.")
         # If no collision occurred but the movement is not finished
         elif self.n < math.ceil(math.fabs(self.delta_rotation / (ANGULAR_SPEED * SAMPLE_TIME))):
-            # DEBUGGING
-            print(type(self).__name__, "returned", ExecutionStatus.RUNNING)
             return ExecutionStatus.RUNNING
         # If the roomba was able to execute the entire movement without colliding, return SUCCESS
         elif self.n >= math.ceil(math.fabs(self.delta_rotation / (ANGULAR_SPEED * SAMPLE_TIME))):
-            print(type(self).__name__, "returned", ExecutionStatus.SUCCESS)
             self.n = 0
             self.status = NodeStatus.VISITED
             return ExecutionStatus.SUCCESS
         else:
             raise RuntimeError("Error! Did not enter in any of the conditions.")
-
-
